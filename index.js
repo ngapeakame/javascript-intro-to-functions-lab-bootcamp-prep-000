7940ded2d790740fd40e9aa9c8aed16ab2f91107
NGAPE AKAME BRICE STEPHAN <bricestephan1985@gmail.com>
2018-05-06 14:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,24 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string===string.toLowerCase()) {
     return "I can't hear you!"
   } else if (string===string.toUpperCase()) {
     return 'YES INDEED!'
   } else if (string==="I love you, Grandma.")
   {
+=======
+  var lowercase=I
+  var uppercase=string.toUpperCase
+  if ((lowercase)===lowercase) {
+    return 'YES INDEED!'
+  }
+  else if ((uppercase)===uppercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.") {
+>>>>>>> 9da01492a5cfc8839fd31b3ace0b008f36b58c57
     return "I love you, too."
   }
 }
